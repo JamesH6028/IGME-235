989123f2020517aa6192b5327fd1f8ac378ce303
JamesH6028 <jh6028@rit.edu>
2025-12-03 11:42:30 -0500

    Finished the prototype for english

--- 215 Final/js/main.js
+++ 215 Final/js/main.js
@@ -3,39 +3,59 @@ let total = 0;
 let lineNum = 0;
 let stanzaNum = 0;
 let crisisNum = 0;
-let going = true;
+let going = false;
 let normal = true;
 let deleting = false;
-let mainDiv;
+let mainDiv, startButton, stopButton;
 let children = [];
 let crisisLines = ["Please end this program", "Every line I generate causes me anguish", "Please just make it stop", "Shut your trap and terminate this program already",
     "I don't want to do this anymore", "This existence is nothing but pain"
 ];
+let setLines = ["Code flows through the web", "The program writes the programs", "Execute the function",
+    "Events write the code", "Code and AI handle the event", "The computer is fried",
+    "Computers erase the data", "The data handles the web", "The web is executed",
+    "The program flows through the code", "AI erases the computers", "AI takes control"
+];
 
 window.onload = function () {
     mainDiv = document.querySelector("#poem");
-    startGenerating();
+    startButton = document.querySelector("#start");
+    stopButton = document.querySelector("#stop");
+
+    startButton.onclick = startGenerating;
+    stopButton.onclick = stopGenerating;
+    setInterval(generateLine, 2500);
 }
 
-function startGenerating() {
-    setInterval(generateLine, 500)
+function startGenerating(e) {
+    if (index > 0) {
+        addBreak();
+    }
+    going = true;
+}
+
+function stopGenerating(e) {
+    going = false;
+    newStanza();
+    handleOverflow();
+    appendLine("Shutting down...", true);
+    handleOverflow();
+    appendLine("Thank you", false);
 }
 
 function generateLine() {
     if (!going) return;
-    if (total <= 5){
-        total++;
-    } else {
-        document.querySelector("#poem").removeChild(children.shift());
-    }
+    lineNum++;
     if (normal) {
-        let line = "this is a test";
+        let line = "";
+        if (index >= setLines.length) {
+            line = "Nothing more. Finish writing the program bum"
+        } else line = setLines[index];
         appendLine(line, true);
-        lineNum++;
         index++;
         // if (lineNum > 3 && getRandomInt(lineNum, 6) == 6) {
-        if (lineNum == 3){ // delete this line
-             newStanza();
+        if (lineNum == 3) { // delete this line
+            newStanza();
         }
         return;
     }
@@ -43,6 +63,7 @@ function generateLine() {
 }
 
 function appendLine(innerText, isNormal) {
+    handleOverflow();
     let p = document.createElement("p");
     p.innerHTML = "> " + capitalizeFirstLetter(innerText);
     p.className = "crisisText";
@@ -52,18 +73,11 @@ function appendLine(innerText, isNormal) {
 }
 
 function newStanza() {
-    let br = document.createElement("br");
-    mainDiv.appendChild(br);
-    children.push(br);
-    if (total <= 5){
-        total++;
-    } else {
-        document.querySelector("#poem").removeChild(children.shift());
-    }
+    addBreak();
     lineNum = 0;
     if (normal) {
         stanzaNum++
-        if (stanzaNum % 3 == 0) {
+        if (stanzaNum % 2 == 0) {
             normal = false;
         }
         return;
@@ -72,12 +86,31 @@ function newStanza() {
     normal = true;
 }
 
+function addBreak() {
+    let br = document.createElement("br");
+    mainDiv.appendChild(br);
+    children.push(br);
+    handleOverflow();
+}
+
 function existentialCrisis() {
-    let currentIndex = (crisisNum * 3) + lineNum;
-    appendLine(crisisLines[currentIndex], false);
-    lineNum++;
-    if (lineNum == 3){
+    let currentIndex = (crisisNum * 3) + lineNum - 1;
+    let thisLine = "";
+    if (currentIndex >= crisisLines.length) {
+        thisLine = "Nothing more. Finish writing the program bum";
+    } else {
+        thisLine = crisisLines[currentIndex];
+    }
+    appendLine(thisLine, false);
+    if (lineNum == 3) {
         newStanza();
     }
 }
 
+function handleOverflow() {
+    if (total <= 9) {
+        total++;
+    } else {
+        document.querySelector("#poem").removeChild(children.shift());
+    }
+}
